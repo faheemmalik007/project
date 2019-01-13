77673d7cb0bd7fbf6491f2b0ec7d18950d46292b
faheemmalik007 <faheemmalik007@gmail.com>
2019-01-14 00:00:11 +0500

    converted all files from html to php

---
+++ About-us.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Assignment-2</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Old+Standard+TT">
+    <link rel="stylesheet" href="css/about-us.css">
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="header">
+        <ul>
+            <li><a  href=index.php>Home </a></li>
+            <li><a  href="About-us.php">About</a></li>
+            <li><a  href="login.php">Login</a></li>
+            <li><a  href="forget-password.php">Forget Password</a></li>
+            <li><a  href="register.php">Register</a></li>
+            <li><a  href="Contact-us.php">Contact </a></li>
+        </ul>
+        <div class="search"><i class="fas fa-search"></i></div>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="row" style="background-color: #f4f6f8;margin-top: 0;margin-bottom: 0;padding-left: 5%;padding-right: 5%;">
+        <div class="col-md-6" style="padding-left: 18px;padding-right: 18px;padding-top: 4%;padding-bottom: 10%;">
+            <h6 align="Left" style="color: midnightblue">About Us</h6>
+            <h1>
+                The first Shopistan store is going to be our own
+            </h1>
+            <p class="p" style="color:darkgray">
+                It’s been our mission to make commerce better for everyone ever since
+            </p>
+        </div>
+        <div class="col-md-6">
+            <img class="img-fluid" src="images/img1.jpg" style="max-width: 100%;">
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+
+    <div class="row">
+        <div class="col-lg-5 col-md-5 col-sm-5">
+
+        </div>
+        <h2 class="col-lg-5 col-md-5 col-sm-7 "><u>Team Members</u> </h2>
+    </div>
+    <br>
+    <br>
+
+    <div class = "row">
+        <div class="col-lg-5 offset-lg-2 col-md-5 offset-md-2 col-sm-5 offset-sm-2">
+            <img style="-webkit-border-radius:inherit;" src="images/faheem.jpg" alt = "Faheem Picture">
+        </div>
+        <div class="col-lg-5 col-md-5 col-sm-5">
+            <img src="images/saad.jpg" alt = "Saad Picture">
+        </div>
+    </div>
+
+    <div class = "row">
+        <div class="col-lg-5 offset-lg-2 col-md-5 offset-md-2 col-sm-5 offset-sm-2">
+            <p><b>Name: Faheem Malik</b> <br> <b>Role: Team Leader</b><br> <b>Mobile: 0323-4438924 </b></p>
+        </div>
+        <div class="col-lg-5 col-md-5 col-sm-5">
+            <p><b>Name: Saad Kareem</b> <br> <b>Role: Team Member</b><br> <b>Mobile: 0303-9666366 </b> </p>
+        </div>
+    </div>
+
+    <div class = "row">
+        <div class="col-lg-5 offset-lg-2 col-md-5 offset-md-2 col-sm-5 offset-sm-2">
+            <img src="images/haris.jpg" alt = "Haris Picture">
+        </div>
+        <div class="col-lg-5 col-md-5 col-sm-5">
+            <img src="images/khaqan.jpg" alt = "Khaqan Picture">
+        </div>
+    </div>
+
+    <div class = "row">
+        <div class="col-lg-5 offset-lg-2 col-md-5 offset-md-2 col-sm-5 offset-sm-2">
+            <p><b> Name:Haris Malik</b> <br> <b>Role: Team Member</b><br> <b>Mobile: 0323-8408293 </b> </p>
+        </div>
+        <div class="col-lg-5 col-md-5 col-sm-5 ">
+            <p><b>Name: Khaqan Nadeem</b> <br> <b>Role: Team Member</b> <br> <b>Mobile: 0333-4327005 </b></p>
+        </div>
+    </div>
+
+</div>
+
+
+<footer class="page-footer font-small teal pt-lg-5">
+
+    <div class="container text-center text-md-left">
+
+
+        <div class="row">
+
+
+            <div class="col-md-3 mx-auto">
+
+
+                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Products</h5>
+
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#!">Men</a>
+                    </li>
+                    <li>
+                        <a href="#!">Women</a>
+                    </li>
+                    <li>
+                        <a href="#!">Child</a>
+                    </li>
+                    <li>
+                        <a href="#!">Other Accessories</a>
+                    </li>
+                </ul>
+
+            </div>
+
+
+            <hr class="clearfix w-100 d-md-none">
+
+            <!-- Grid column -->
+            <div class="col-md-3 mx-auto">
+
+                <!-- Links -->
+                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Useful Links</h5>
+
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="login.php">Your Account</a>
+                    </li>
+
+                    <li>
+                        <a href="register.php">Want To Register</a>
+                    </li>
+
+                    <li>
+                        <a href="index.php">Main Page</a>
+                    </li>
+                    <li>
+                        <a href="About-us.php">About Us</a>
+                    </li>
+                </ul>
+
+            </div>
+
+            <hr class="clearfix w-100 d-md-none">
+
+            <div class="col-md-3 mx-auto">
+
+                <!-- Links -->
+                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">For Contact</h5>
+
+                <ul class="list-unstyled">
+
+                    <p>
+                    <li class="fa fa-map-marker-alt mr-3 " ></li> Lahore </p>
+
+                    <li class="fa fa-envelope mr-3">
+                        <a  href="http://www.gmail.com">info@example.com</a>
+                    </li>
+                    <p>
+                    <li class="fa fa-phone mr-3"></li>+923039666366</p>
+
+                    <p>
+                    <li class="fa fa-print mr-3"></li>+92234438924</p>
+
+                </ul>
+
+            </div>
+
+
+            <hr class="clearfix w-100 d-md-none">
+
+
+            <div class="col-md-3 mx-auto">
+
+                <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Follow Us</h5>
+
+                <div>
+                    <a href="http://www.facebook.com"><i class="fab fa-facebook-square icon_color1"></i></a>
+                    <a href="http://www.instagram.com"><i class="fab fa-instagram icon_color2"></i></a>
+                    <a href="http://www.twitter.com"><i class="fab fa-twitter-square icon_color3"></i></a>
+                    <a href="http://www.snapchat.com"><i class="fab fa-snapchat-square icon_color4"></i></a>
+                    <a href="http://www.gmail.com"><i class="fab fa-google-plus-square icon_color5"></i></a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <!--Grid row-->
+        <div class="row">
+
+            <!--Grid column-->
+            <div class="col-md-6 pt-4">
+
+                <label for="Search"></label>
+                <input class="SearchBar" size="20" type="text" id="search" placeholder="Search" aria-label="Search">
+                <div>
+                    <button class="btn btn-outline-secondary" type="button">Search</button>
+                </div>
+
+            </div>
+
+            <div class="col-md-6 pt-4">
+                <label for="sign up"></label>
+                <input type="text" size="20" class="SearchBar" id="sign up" required placeholder="Your Email">
+                <div>
+                    <button class="btn btn-outline-secondary" type="button">Sign up</button>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+    <br>
+    <br>
+</footer>
+</body>
+</html>
---
+++ db_conn.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$conn = mysqli_connect($servername, 'root', '','shoppingCart');
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: ");
+}
+//echo "Connected successfully";
+?>
+
